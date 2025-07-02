f388bae0ecdbf906503d6ab6f47b4fbdbb249e71
kuwakuwa1225 <kiiroi903@gmail.com>
2025-07-02 11:12:50 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -24,7 +24,7 @@ target_time = (now + timedelta(minutes=10)).strftime("%H:%M")
 # 条件に一致する行を検索
 res = supabase.table("subjects") \
     .select("*") \
-    .eq("day", current_weekday) \
+    .eq("day_of_week", current_weekday) \
     .eq("time", target_time) \
     .execute()
 
